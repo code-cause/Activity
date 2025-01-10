d0749b7941ef1e513b1d657f572daaa458b78aa9
Ashish <writeashish22@gmail.com>
2025-01-10 22:33:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,93 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>questions</h1>
+
+    <p>q1.print the pattern</p>
+
+        1
+        <br>
+        121
+        <br>
+        12321
+        <br>
+        1234321
+        <br>
+        
+        
+        
+        
+       <p>q2. print the last  cout of consecutive zero of a number from last dont use string</p> 
+
+    
+          <p>input:  104000 <br>
+          output: 3 </p>
+
+
+           
+
+
+        
+    
+        
+        
+        
+        
+        <p>q3. print the first repeating character of input </p>
+         <p>input:
+            Yashaswini
+            <br>
+            output: a</p>
+        
+        
+        
+        
+         <p>q4. using binary search print the square root of a number</p>
+         <p>input:
+           16
+            <br>
+            output: 4</p>
+        
+        
+        
+       <p>  q5. find the count of 0 & 1 forming the longest sequence(pattern)<br>
+       <p> input:
+        0 0 0 1 1 1 0 0 0 <br>
+        output: 6 <br></p>
+     
+         input: 00001001 <br>
+    
+         output: 4</p>
+        
+        
+        
+        <p>q6. ques toh yaad nhi <br>
+        
+          <p>  input: abcd <br>
+            output: true <br></p>
+            
+           <p> input: aabbcc <br>
+            output: true <br></p>
+            
+          <p>  input: xyz <br>
+            output: true <br></p>
+            
+            input: abaab <br>
+            output: false
+
+            
+            </p>
+        
+        
+        
+        
+        </p>
+</body>
+</html>
